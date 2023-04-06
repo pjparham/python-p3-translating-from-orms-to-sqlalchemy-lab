b96f6ebdf116fac991d36666c7ed74bdea9d91b8
Parker Parham <parkerparham@gmail.com>
2023-04-06 17:50:25 -0400

    Refactored code

--- lib/dog.py
+++ lib/dog.py
@@ -1,5 +1,4 @@
 from models import Dog
-from sqlalchemy import create_engine
 
 def create_table(base, engine):
     base.metadata.create_all(engine)
@@ -9,25 +8,29 @@ def save(session, dog):
     session.commit()
 
 def get_all(session):
-    dogs = session.query(Dog)
-    return [dog for dog in dogs]
+    return session.query(Dog)
+    # dogs = session.query(Dog)
+    # return [dog for dog in dogs]
 
 def find_by_name(session, name):
-    query = session.query(Dog).filter(Dog.name.like(name)).all()
-    print(query)
-    for record in query:
-        return record
+    return session.query(Dog).filter(Dog.name == name).first()
+    # query = session.query(Dog).filter(Dog.name.like(name)).all()
+    # print(query)
+    # for record in query:
+    #     return record
 
 def find_by_id(session, id):
-    query = session.query(Dog).filter(Dog.id == id).all()
-    for record in query: 
-        return record
+    return session.query(Dog).filter(Dog.id == id).first()
+    # query = session.query(Dog).filter(Dog.id == id).all()
+    # for record in query: 
+    #     return record
 
 def find_by_name_and_breed(session, name, breed):
-    query = session.query(Dog).filter(Dog.name.like(name),
-        Dog.breed == breed).all()
-    for record in query:
-        return record
+    return session.query(Dog).filter(Dog.name.like(name), Dog.breed == breed).first()
+    # query = session.query(Dog).filter(Dog.name.like(name),
+    #     Dog.breed == breed).all()
+    # for record in query:
+    #     return record
 
 def update_breed(session, dog, breed):
     dog_to_update = session.query(Dog).filter(Dog.name.like(dog.name)).first()
